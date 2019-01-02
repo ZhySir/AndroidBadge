e8195143fbcbfa7e6e61765619d816c783f18500
zhysir <zhysir888@163.com>
2019-01-02 11:56:43 +0800

    Travis CI

--- app/src/main/java/com/zhy/androidbadge/MainActivity.java
+++ app/src/main/java/com/zhy/androidbadge/MainActivity.java
@@ -2,12 +2,36 @@ package com.zhy.androidbadge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.zhy.libbadges.Utils;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        tvHelloWorld = findViewById(R.id.tv_HelloWorld);
+
+        tvHelloWorld.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_HelloWorld:
+                Toast.makeText(this, "编程第一行代码：" + Utils.getCode(), Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
